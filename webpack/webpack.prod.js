37186dd701724df717680f1064022f8783425028
hzz <383686357@qq.com>
2021-11-20 19:22:20 +0800

    chore: webpack配置调整

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const common = require("./webpack.base.js");
 const webpack = require("webpack");
 
 const TerserPlugin = require('terser-webpack-plugin');
